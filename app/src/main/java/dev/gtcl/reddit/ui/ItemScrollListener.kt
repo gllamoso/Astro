72a6bc4478c46da19c9d491af846f2a9ce58ee8e
Gerard <gerardllamoso@gmail.com>
2020-05-23 21:26:06 -0700

    Revert: Reinstated ItemScrollListener

---
+++ app/src/main/java/dev/gtcl/reddit/ui/ItemScrollListener.kt
@@ -0,0 +1,35 @@
+package dev.gtcl.reddit.ui
+
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemScrollListener(private val visibleThreshold: Int = 15, private val layoutManager: GridLayoutManager, private val loadMore: () -> Unit) : RecyclerView.OnScrollListener() {
+
+    private var isLoading = false
+    fun finishedLoading(){
+        isLoading = false
+    }
+
+    private var hasReachedLastItem = false
+    fun lastItemReached(){
+        hasReachedLastItem = true
+    }
+
+    fun reset(){
+        hasReachedLastItem = false
+    }
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        if(dy <= 0 || hasReachedLastItem) return
+
+        val totalItemCount = layoutManager.itemCount
+        val lastVisibleItem = layoutManager.findLastVisibleItemPosition()
+        if(!isLoading && totalItemCount <= lastVisibleItem + visibleThreshold){
+            loadMore()
+            isLoading = true
+        }
+    }
+
+
+}
