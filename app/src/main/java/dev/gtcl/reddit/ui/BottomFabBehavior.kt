59ff1aa0abcf11a4f55bd9d510e0a9c3a715c97f
Gerard <gerardllamoso@gmail.com>
2020-08-04 22:49:53 -0700

    Added FAB in InboxFragment and a style for TabLayouts

---
+++ app/src/main/java/dev/gtcl/reddit/ui/BottomFabBehavior.kt
@@ -0,0 +1,50 @@
+package dev.gtcl.reddit.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import kotlin.math.max
+import kotlin.math.min
+
+class BottomFabBehavior<V : View>(context: Context, attrs: AttributeSet) :
+    CoordinatorLayout.Behavior<V>(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout, child: V, directTargetChild: View, target: View, axes: Int, type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout, child: V, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        child.translationY = max(0f, min((child.height.toFloat() * 1.5).toFloat(), child.translationY + dy))
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            0,
+            0,
+            type,
+            consumed
+        )
+    }
+}
