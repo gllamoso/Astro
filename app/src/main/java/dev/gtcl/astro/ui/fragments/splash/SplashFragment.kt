83890a55f1a485ab1124b4bdd15f0875e1e0fb7a
Gerard <gerardllamoso@gmail.com>
2020-09-01 18:16:58 -0700

    Added step of syncing subscriptions after loading in the SplashFragment.kt

--- app/src/main/java/dev/gtcl/astro/ui/fragments/splash/SplashFragment.kt
+++ app/src/main/java/dev/gtcl/astro/ui/fragments/splash/SplashFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.gson.Gson
@@ -13,6 +14,7 @@ import dev.gtcl.astro.*
 import dev.gtcl.astro.database.SavedAccount
 import dev.gtcl.astro.databinding.FragmentSplashBinding
 import dev.gtcl.astro.models.reddit.listing.FrontPage
+import dev.gtcl.astro.ui.activities.MainActivityVM
 import dev.gtcl.astro.ui.fragments.ListingPage
 
 class SplashFragment : Fragment(){
@@ -23,6 +25,8 @@ class SplashFragment : Fragment(){
         val viewModelFactory = ViewModelFactory(requireActivity().application as AstroApplication)
         ViewModelProvider(this, viewModelFactory).get(SplashVM::class.java)
     }
+
+    private val activityModel: MainActivityVM by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,10 +40,9 @@ class SplashFragment : Fragment(){
 
         model.ready.observe(viewLifecycleOwner, {
             if(it != null){
-                findNavController().navigate(SplashFragmentDirections.actionSplashScreenFragmentToViewPagerFragment(ListingPage(
-                    FrontPage
-                )))
+                findNavController().navigate(SplashFragmentDirections.actionSplashScreenFragmentToViewPagerFragment(ListingPage(FrontPage)))
                 model.readyComplete()
+                activityModel.syncSubscriptionsWithReddit()
             }
         })
 
