4033162d3dc37658ce5aecf964d6aba0988fa547
Gerard <gerardllamoso@gmail.com>
2020-09-12 00:03:13 -0700

    Increased thumbnail size for albums and skipped memory caching when loading thumbnails with Glide

--- app/src/main/java/dev/gtcl/astro/ui/viewholders/MediaVH.kt
+++ app/src/main/java/dev/gtcl/astro/ui/viewholders/MediaVH.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import dev.gtcl.astro.MediaType
 import dev.gtcl.astro.databinding.ItemMediaSelectableBinding
@@ -25,19 +26,23 @@ class MediaVH private constructor(private val binding: ItemMediaSelectableBindin
     }
 
     private fun setImage(context: Context, url: String) {
-        val requestOptions = RequestOptions().fitCenter().override(64, 64)
+        val requestOptions = RequestOptions().fitCenter().override(128, 128)
 
         Glide.with(context).load(url)
             .apply(requestOptions)
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(binding.itemMediaSelectableImage)
     }
 
     private fun setVideoPreviewImage(context: Context, url: String) {
         val thumb = layoutPosition * 1000.toLong()
-        val requestOptions = RequestOptions().fitCenter().override(64, 64)
+        val requestOptions = RequestOptions().fitCenter().override(128, 128)
         val options = RequestOptions().frame(thumb)
         Glide.with(context).load(url).apply(options)
             .apply(requestOptions)
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(binding.itemMediaSelectableImage)
     }
 
